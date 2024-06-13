bbc171d59cd457d6f075eee1516b625989af80fa
CRPragyan <pragyanchakravarty57@gmail.com>
2024-06-13 20:46:37 +0530

    Updated coursepage

--- src/pages/CourseSingle.jsx
+++ src/pages/CourseSingle.jsx
@@ -1,8 +1,30 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { CardData } from "../assets/Data";
 
 function CourseSingle() {
   const { id } = useParams();
-  return <div>Place Page: {id}</div>;
+  const [course, setCourse] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const foundCourse = CardData.find(course => course.id === id);
+    setCourse(foundCourse);
+  }, [id]);
+
+  if (!course) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{course.title}</h1>
+      <p>{course.name}</p>
+      <p>Duration: {course.duration}</p>
+      <img src={course.img} alt={course.name} />
+    </div>
+  );
 }
 
 export default CourseSingle;
