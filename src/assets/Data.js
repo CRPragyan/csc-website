9ddab2164ae94aa7191fc9e3d6e209ee2197ea6e
CRPragyan <pragyanchakravarty57@gmail.com>
2024-06-13 13:58:49 +0530

    Update Data

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -42,6 +42,9 @@ const slides = [
   {
     url: "https://images.unsplash.com/photo-1573164574572-cb89e39749b4?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
+  {
+    url: "https://images.unsplash.com/photo-1565598621680-94ac0c22b148?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 ];
 
 export { CardData, slides };
