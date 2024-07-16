f3c29a820b4675f9ee016d14bfd774e7db0c0f3c
CRpragyan27 <pragyan27c@gmail.com>
2024-07-16 16:14:02 +0530

    Adding Photos

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -1,19 +1,21 @@
 const slides = [
   {
-    url: "/img/photo3.jpg",
+    url: "/img/img1.jpg",
   },
   {
-    url: "/img/photo2.jpg",
+    url: "/img/img2.jpg",
   },
   {
-    url: "/img/photo1.jpeg",
+    url: "/img/img3.jpg",
   },
   {
-    url: "https://images.unsplash.com/photo-1573164574572-cb89e39749b4?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/img/img4.jpg",
   },
   {
-    url: "https://images.unsplash.com/photo-1565598621680-94ac0c22b148?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/img/img5.jpg",
   },
+  
+  
 ];
 
 export { slides };
