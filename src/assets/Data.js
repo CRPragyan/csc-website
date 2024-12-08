9e8ac99ccf00a21df7833542f9cbda06ac103ccd
CRPragyan <pragyanchakravarty57@gmail.com>
2024-12-08 17:50:44 +0530

    adding photo

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -9,13 +9,11 @@ const slides = [
     url: "/img/img3.jpg",
   },
   {
-    url: "/img/img4.jpg",
-  },
-  {
     url: "/img/img5.jpg",
   },
-  
-  
+  {
+    url: "/img/img6.jpg",
+  },
 ];
 
 export { slides };
